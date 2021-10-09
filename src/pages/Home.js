025c5c4c5ee3003963d6b64f4ab58fb03bf5045e
Busiwa24 <busiwaliuma@gmail.com>
2021-10-09 18:58:14 +0200

    add content to home JS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,21 +7,12 @@ const Home = () => (
     <div className="container">
       <h1>Welcome to our page!</h1>
       <p>
-        Math magicians is a website for all fans of mathematics. It is a Single
-        Page App (SPA) that allows users to: Make simple calculations. Read a
-        random math-related quote.
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos, fugit
-        odit molestiae praesepossimus. Veniam, architecto. Labore doloremque
-        illo minus odit nulla nesciunt eius omnis, adipisci, possimus
-        quosexercitationem. Accusamus praesentium, qui minus quam ipsa, illo
-        reprehenderit maxime deserunt aut, iste cupiditate est repellendus natus
-        doloremque sapiente accusantium obcaecati fugiat facere suscipit?
-        Dolores ducimus quis, laborum esse assumenda autem. Enim aut, ullam, ex
-        maxime commodi minima nemo ea, consequatur dignissimos dolore quo
-        necessitatibus officiis voluptatum voluptates cum! Iste dolorum
-        obcaecati ipsam incidunt distinctio debitis a, atque consectetur dolores
-        amet! Molestiae impedit cupiditate nam non quibusdam, harum eligendi
-        doloribus magnam ea.
+    Did you know that nature loves Fibonacci sequences? The spiral shapes of sunflowers and other patterns in nature follow a Fibonacci sequence, 
+    where adding the two preceding numbers in the sequence gives you the next (1, 1, 2, 3, 5, 8, etc.)
+    And If you multiply 111,111,111 × 111,111,111 you get 12,345,678,987,654,321 - a palindrome number that reads the same forwards or backwards.
+    And that works all the way back down to 11 x 11 (121) or just 1 x 1 (1).
+    In a crowded room probably two people share a birthday.It only takes 23 people to enter a room to give you an evens chance that two of them have the same birthday.
+   With 75 people in the room the chances rise to 99 per cent!
       </p>
     </div>
   </Route>

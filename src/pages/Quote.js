36353ef20af472fbbda3907b304d5178a5c72c0b
Busiwa24 <busiwaliuma@gmail.com>
2021-10-09 19:20:51 +0200

    add content in pages

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -4,13 +4,13 @@ import React from 'react';
 const Quote = () => (
   <div className="container">
     <h3>
-    On a beautiful wall, there was a wall of math- Josh Lucas
+      On a beautiful wall, there was a wall of math- Josh Lucas
     </h3>
     <h3>
-    Life is only good for two things; discovering mathematics and teaching mathematics. -Simeon Dennis Poisson
+      Life is only good for two things; discovering mathematics and teaching mathematics. -Simeon Dennis Poisson
     </h3>
     <h3>
-    Beauty is the first test; there is no permanent place in the world for ugly mathematics. -G.H Hardy
+      Beauty is the first test; there is no permanent place in the world for ugly mathematics. -G.H Hardy
     </h3>
   </div>
 );
